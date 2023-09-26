c23e3f8ca2ec227754682fcb2e8d40b4d588655c
franyatta <95552724+franyatta@users.noreply.github.com>
2023-09-26 13:37:20 -0400

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,30 @@
+// This program will tell you if a word is a palindrome or not
+import java.util.Scanner;
+public class Palindrome {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String s = input.nextLine();
+		
+		int low = 0;
+		int high = s.length() - 1;
+		
+		boolean isPalindrome = true;
+		while (low < high) {
+			if (s.charAt(low) != s.charAt(high)) {
+				isPalindrome = false;
+				break;
+			}
+			
+			low++;
+			high--;
+		}
+		if (isPalindrome)
+			System.out.println(s + " is a palindrome");
+		else
+			System.out.println(s + " is not a palindrome");
+		input.close();
+	}
+}
+// franyatta
